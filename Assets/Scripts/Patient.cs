78489a09f2149fa5165a4371337b7581b6463085
non0n0no <a55120@alunos.ipb.pt>
2024-12-16 22:31:12 +0000

    Creating a Multi-Step Plan

--- Assets/Scripts/Patient.cs
+++ Assets/Scripts/Patient.cs
@@ -1,14 +1,15 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Patient : GAgent
 {
     // Start is called before the first frame update
-    protected override void Start()
+    new void Start()
     {
         base.Start();
         SubGoal s1 = new SubGoal("isWaiting", 1, true);
-        goals.Add(s1, 3);
+        goals.Add(s1, 3); 
     }
+
 }
--- Assets/Scripts/Register.cs
+++ Assets/Scripts/Register.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GoToHospital : GAction
+public class Register : GAction
 {
     public override bool PrePerform()
     {
